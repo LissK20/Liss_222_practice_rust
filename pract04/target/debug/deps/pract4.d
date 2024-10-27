42db338fa51d7c271d9997e74273e3969e5e29c7
LissK20 <k.liss@e-u.edu.ua>
2024-10-27 15:01:16 +0200

    Add files via upload

---
+++ pract04/target/debug/deps/pract4.d
@@ -0,0 +1,5 @@
+E:\ЄУ\2 курс\Rust\rust_2xx_pract_Liss\pract4\target\debug\deps\pract4.exe: src/main.rs
+
+E:\ЄУ\2 курс\Rust\rust_2xx_pract_Liss\pract4\target\debug\deps\pract4.d: src/main.rs
+
+src/main.rs:
---
+++ pract04/target/debug/pract4.d
@@ -0,0 +1,1 @@
+E:\ЄУ\2\ курс\Rust\rust_2xx_pract_Liss\pract4\target\debug\pract4.exe: E:\ЄУ\2\ курс\Rust\rust_2xx_pract_Liss\pract4\src\main.rs
